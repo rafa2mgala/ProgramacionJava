1d0c91c81bdae84813734176adf1ce70aed7e08e
rafa2mgala <rmenmar264@g.educaand.es>
2022-11-28 18:14:02 +0100

    Soluciones propuestas a la actividad 2.3

--- ProgramacionJava/src/dam/temados/poo/cadenas/Caracter.java
+++ ProgramacionJava/src/dam/temados/poo/cadenas/Caracter.java
@@ -3,12 +3,11 @@ package dam.temados.poo.cadenas;
 import java.util.Scanner;
 
 public class Caracter {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce un número entre 0 y 255:");
 		int numero = sc.nextInt();
-		System.out.println((char)numero);
+		System.out.println(String.format("Caracter: %c", numero));
+		sc.close();
 	}
-
 }
