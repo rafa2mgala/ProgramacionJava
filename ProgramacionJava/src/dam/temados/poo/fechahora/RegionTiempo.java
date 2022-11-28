f1390638c3c5293409360f08e51c37d11829246b
rafa2mgala <rmenmar264@g.educaand.es>
2022-11-28 21:39:09 +0100

    Solución propuesta a la actividad 2.8

--- ProgramacionJava/src/dam/temados/poo/fechahora/RegionTiempo.java
+++ ProgramacionJava/src/dam/temados/poo/fechahora/RegionTiempo.java
@@ -3,6 +3,7 @@ package dam.temados.poo.fechahora;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 /**
@@ -41,5 +42,39 @@ public class RegionTiempo {
 		//se regionaliza el tiempo a Canarias
 		System.out.println("Fecha/Hora en Canarias " + 
 				ZonedDateTime.ofInstant(ahora, tiempoZona));
+
+		//se crea la zona de Europa
+		//américa tiene 4 husos horarios, desde 0 hasta +3
+		//a continuación sólo se mostrará la fecha y hora en
+		//Londres
+		tiempoZona = ZoneId.ofOffset("GMT",ZoneOffset.ofHours(0));
+		//se regionaliza el tiempo de Londres
+		System.out.println("Fecha/Hora en Londres " + 
+				ZonedDateTime.ofInstant(ahora, tiempoZona));
+
+		//se crea la zona de América
+		//américa tiene 10 husos horarios, desde -2 hasta -9
+		//a continuación sólo se mostrará la fecha y hora en
+		//New York
+		tiempoZona = ZoneId.ofOffset("GMT",ZoneOffset.ofHours(-5));
+		//se regionaliza el tiempo de América
+		System.out.println("Fecha/Hora en New York " + 
+				ZonedDateTime.ofInstant(ahora, tiempoZona));
+
+		//se crea la zona de Antartida
+		tiempoZona = ZoneId.ofOffset("GMT",ZoneOffset.ofHours(13));
+		//se regionaliza el tiempo a la Antartida
+		System.out.println("Fecha/Hora en la Antartida " + 
+				ZonedDateTime.ofInstant(ahora, tiempoZona));
+
+		//se crea la zona de Oceanía
+		tiempoZona = ZoneId.ofOffset("GMT",ZoneOffset.ofHours(11));
+		//se regionaliza el tiempo en Oceanía
+		System.out.println("Fecha/Hora en Oceanía " + 
+				ZonedDateTime.ofInstant(ahora, tiempoZona));
+
+		//igual para Asia y África, se buscan los husos horarios
+		//y se asignan mediante offsets
+
 	}
 }
